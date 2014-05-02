b6e10e77d01da6f53a3fa2ba6a42547d9f73905a
awtauer <atauer2015@kellogg.northwestern.edu>
2014-05-01 22:04:17 -0500

    API#1
    Part1

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -6,11 +6,20 @@ class AddressesController < ApplicationController
     @address = "the corner of Foster and Sheridan"
     @url_safe_address = URI.encode(@address)
 
-    # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @latitude = ?
-    # @longitude = ?
+    #result = JSON.parse(open(http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+Foster+and+Sheridan&sensor=false))
+
+
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+Foster+and+Sheridan&sensor=false"
+
+    raw_data = open(url).read
+
+    parsed_data = JSON.parse(raw_data)
+    results = parsed_data["results"]
+    first = results[0]
+    geometry = first["geometry"]
+    location = geometry["location"]
+    @latitude = location["lat"]
+    @longitude = location["lng"]
+
   end
 end
